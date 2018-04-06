fb116fc25981c3adcdb2e2d2ec64e7087c33b447
Newton Kiragu <karanunewton4@gmail.com>
2018-04-06 10:47:05 +0300

    added functionality for other consoles

--- install.sh
+++ install.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
 
 function addbashaliases() {
-  # Add source bash_aliases on .bashrc
-cat >> ~/.bashrc << EOT
-if [ -f ~/aliases/.bash_aliases ]; then
-  . ~/aliases/.bash_aliases
-fi
+if [ -f $HOME/.bashrc ]; then
+cat >> $HOME/.bashrc << EOT
+  source $HOME/aliases/.bash_aliases
 EOT
-
-# Reload current environment
-  source ~/.bashrc
+source $HOME/.bashrc
+elif [ -f $HOME/.zshrc ]; then
+cat >> $HOME/.zshrc << EOT
+  source $HOME/aliases/.bash_aliases
+EOT
+  source $HOME/aliases/.bash_aliases
+elif [ -f $HOME/.bash_profile ]; then
+cat >> $HOME/.bash_profile << EOT
+  source $HOME/aliases/.bash_aliases
+EOT
+  source $HOME/aliases/.bash_aliases
+else
+  echo 'no console has been found! Please contact the developers for more details'
+fi
 }
 
 # Execute the function
