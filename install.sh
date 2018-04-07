c94a72055abacfd3ef3544d81787ca18f304fd69
Newton Kiragu <karanunewton4@gmail.com>
2018-04-07 14:30:10 +0300

    fixed bug where the code added to the terminal is not added to a new line

--- install.sh
+++ install.sh
@@ -3,16 +3,22 @@
 function addbashaliases() {
 if [ -f $HOME/.bashrc ]; then
 cat >> $HOME/.bashrc << EOT
+
+  # location of bash aliases
   source $HOME/aliases/.bash_aliases
 EOT
 source $HOME/.bashrc
 elif [ -f $HOME/.zshrc ]; then
 cat >> $HOME/.zshrc << EOT
+
+  # location of bash aliases
   source $HOME/aliases/.bash_aliases
 EOT
   source $HOME/aliases/.bash_aliases
 elif [ -f $HOME/.bash_profile ]; then
 cat >> $HOME/.bash_profile << EOT
+
+  # location of bash aliases
   source $HOME/aliases/.bash_aliases
 EOT
   source $HOME/aliases/.bash_aliases
