4b5c5da6f8b9c3667d88a2dd096fcf6ab3d016d0
newtonkiragu <karanunewton4@gmail.com>
2018-08-24 15:37:47 +0300

    updated install script to reload terminal after

--- run.sh
+++ run.sh
@@ -5,4 +5,19 @@ git clone https://github.com/newtonkiragu/aliases/
 cd aliases
 chmod +x install.sh
 ./install.sh
+
+function reload() {
+if [ -f $HOME/.bashrc ]; then
+source $HOME/.bashrc
+elif [ -f $HOME/.zshrc ]; then
+ source $HOME/aliases/.bash_aliases
+elif [ -f $HOME/.bash_profile ]; then
+source $HOME/aliases/.bash_aliases
+else
+  echo 'please reload your terminal fam.'
+fi
+}
+
+# Execute the function
+reload
 cd
