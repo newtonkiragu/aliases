2e40ccdf284e4a26866f6499d488d604544d687e
newtonkiragu <karanunewton4@gmail.com>
2018-08-25 14:59:24 +0300

    updated installation script to confirm installation of aliases

--- install.sh
+++ install.sh
@@ -5,21 +5,33 @@ if [ -f $HOME/.bashrc ]; then
 cat >> $HOME/.bashrc << EOT
 
 # location of bash aliases
+if [ -f $HOME/aliases/.bash_aliases]; then
 source $HOME/aliases/.bash_aliases
+else
+echo 'please re-install bash aliases from https://github.com/newtonkiragu/aliases'
+fi
 EOT
 source $HOME/.bashrc
 elif [ -f $HOME/.zshrc ]; then
 cat >> $HOME/.zshrc << EOT
 
 # location of bash aliases
+if [ -f $HOME/aliases/.bash_aliases]; then
 source $HOME/aliases/.bash_aliases
+else
+echo 'please re-install bash aliases from https://github.com/newtonkiragu/aliases'
+fi
 EOT
 source $HOME/.zshrc
 elif [ -f $HOME/.bash_profile ]; then
 cat >> $HOME/.bash_profile << EOT
 
 # location of bash aliases
+if [ -f $HOME/aliases/.bash_aliases]; then
 source $HOME/aliases/.bash_aliases
+else
+echo 'please re-install bash aliases from https://github.com/newtonkiragu/aliases'
+fi
 EOT
 source $HOME/.bash_profile
 else
