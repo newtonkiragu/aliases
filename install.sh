f4ff183ce3672f16f9d9c6637f969090fa820c10
Newton Kiragu <karanunewton4@gmail.com>
2019-01-22 21:00:54 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,8 +5,8 @@ if [ -f $HOME/.bashrc ]; then
 cat >> $HOME/.bashrc << EOT
 
 # location of bash aliases
-if [ -f $HOME/aliases/.bash_aliases ]; then
-source $HOME/aliases/.bash_aliases
+if [ -f $HOME/.aliases/.bash_aliases ]; then
+source $HOME/.aliases/.bash_aliases
 else
 echo '
    #    #       ###    #     #####  #######  #####  
@@ -26,8 +26,8 @@ elif [ -f $HOME/.zshrc ]; then
 cat >> $HOME/.zshrc << EOT
 
 # location of bash aliases
-if [ -f $HOME/aliases/.bash_aliases ]; then
-source $HOME/aliases/.bash_aliases
+if [ -f $HOME/.aliases/.bash_aliases ]; then
+source $HOME/.aliases/.bash_aliases
 else
 echo '
    #    #       ###    #     #####  #######  #####  
@@ -39,7 +39,7 @@ echo '
 #     # ####### ### #     #  #####  #######  #####  
                                                     
 '
-echo 'please re-install bash aliases from https://github.com/newtonkiragu/aliases'
+echo 'please re-install bash aliases from https://github.com/newtonkiragu/.aliases'
 fi
 EOT
 source $HOME/.zshrc
@@ -47,8 +47,8 @@ elif [ -f $HOME/.bash_profile ]; then
 cat >> $HOME/.bash_profile << EOT
 
 # location of bash aliases
-if [ -f $HOME/aliases/.bash_aliases ]; then
-source $HOME/aliases/.bash_aliases
+if [ -f $HOME/.aliases/.bash_aliases ]; then
+source $HOME/.aliases/.bash_aliases
 else
 echo '
    #    #       ###    #     #####  #######  #####  
@@ -60,7 +60,7 @@ echo '
 #     # ####### ### #     #  #####  #######  #####  
                                                     
 '
-echo 'please re-install bash aliases from https://github.com/newtonkiragu/aliases'
+echo 'please re-install bash aliases from https://github.com/newtonkiragu/.aliases'
 fi
 EOT
 source $HOME/.bash_profile
